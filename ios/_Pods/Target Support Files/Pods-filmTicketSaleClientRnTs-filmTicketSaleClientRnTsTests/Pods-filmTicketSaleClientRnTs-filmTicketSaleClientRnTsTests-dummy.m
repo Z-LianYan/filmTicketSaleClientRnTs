1458de163619dfceadcf9dacd14cabb64729b3ef
zly <1371935318@qq.com>
2022-06-13 01:02:16 +0800

    1

--- ios/_Pods/Target Support Files/Pods-filmTicketSaleClientRnTs-filmTicketSaleClientRnTsTests/Pods-filmTicketSaleClientRnTs-filmTicketSaleClientRnTsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_filmTicketSaleClientRnTs_filmTicketSaleClientRnTsTests : NSObject
-@end
-@implementation PodsDummy_Pods_filmTicketSaleClientRnTs_filmTicketSaleClientRnTsTests
-@end
